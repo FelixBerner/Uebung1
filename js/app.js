666020150afb95459b95192aade782d5d3fdcf6b
FelixBerner <felix.berner1@web.de>
2014-09-08 13:36:36 +0200

    Uebung1
    uebung mit AngularJs und Bootstrap

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+//Define an angular module for our app
+var app = angular.module('app', ['ngRoute', 'ngResource']);
+ 
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/seite1', {
+        templateUrl: 'partials/seite1.html',
+        controller: 'InhaltCtrl'
+    }).
+      when('/seite2', {
+        templateUrl: 'partials/seite2.html',
+        controller: 'ListeCtr'
+      }).
+      otherwise({
+        redirectTo: '/seite1'
+      });
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,56 @@
+'use strict';
+
+app.controller('ListeCtr', ['$scope', '$routeParams',
+  function($scope) {
+    //Personenliste erstellen
+    $scope.persons = [];
+
+    $scope.addPerson = function() {
+      //Daten in die Liste eintragen
+      $scope.persons.push({
+        vorName: $scope.newVName,
+        nachName: $scope.newNName,
+        gebDatum: $scope.newGebDat
+      });
+      //Eingabefelder zurücksetzen
+      $scope.newVName = '';
+      $scope.newNName = '';
+      $scope.newGebDat = '';
+    }
+  }
+]);
+
+app.controller('InhaltCtrl', function($scope, $http) {
+  $http.get('inhalt/jumbos.json').then(function(contentResponse) {
+    console.log(contentResponse);
+    $scope.contents = contentResponse.data.jumbos;
+
+
+    $scope.next = function(e) {
+      console.log('next');
+      $('.carousel').carousel('next');
+    }
+    $scope.prev = function(e) {
+      console.log('previous');
+      $('.carousel').carousel('prev');
+    }
+
+    $('.carousel').carousel();
+
+    $scope.active = function(first) {
+      if (first) {
+        return "active";
+      } else {
+        return "";
+      }
+    }
+
+  });
+
+});
+
+
+
+
+
+
